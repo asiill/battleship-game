ab300ed78bbaf4c56c18ec2589f736349ae61d42
asil <9745019+asiill@users.noreply.github.com>
2023-09-25 13:51:33 -0400

    Update gameboard.js

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,8 +1,8 @@
 import { Ship } from "./ship.js";
 
-export const Gameboard = (n) => {
-    let size = n;
-    let board = Array(size).fill("").map(() => Array(size).fill(""));
+export const Gameboard = (size) => {
+    let n = size;
+    let board = Array(n).fill("").map(() => Array(n).fill(""));
     let ships = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)];
 
     const getBoard = () => {
@@ -10,7 +10,72 @@ export const Gameboard = (n) => {
     };
 
     const reset = () => {
-        board = Array(size).fill("").map(() => Array(size).fill(""));
+        board = Array(n).fill("").map(() => Array(n).fill(""));
+    };
+
+    const isValidPosition = (length, direction, x, y) => {
+        // if direction is horizontal
+        if (direction === 1) {
+            for (let i = 0; i < length; i++) {
+                if ((x + i) > 9) {
+                    return false;
+                }
+            }
+        }
+        // else if direction is vertical
+        else {
+            for (let i = 0; i < length; i++) {
+                if ((y + i) > 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
+
+    const placeShip = (ship, direction, x, y) => {
+        let length = ship.getLength();
+        if (isValidPosition(length, direction, x, y)) {
+            // if direction is horizontal
+            if (direction === 1) {
+                for (let i = 0; i < length; i++) {
+                    board[x + i][y] = "x";
+                }
+            }
+            // else if direction is vertical
+            else {
+                for (let i = 0; i < length; i++) {
+                    board[x][y + i] = "x";
+                }
+            }
+        }
+        else {
+            // if direction is horizontal
+            if (direction === 1) {
+                x = x - ((x + (length - 1)) - (n - 1));
+                for (let i = 0; i < length; i++) {
+                    board[x + i][y] = "x";
+                }
+            }
+            // else if direction is vertical
+            else {
+                y = y - ((y + (length - 1)) - (n - 1));
+                for (let i = 0; i < length; i++) {
+                    board[x][y + i] = "x";
+                }
+            }
+        }
+    };
+
+    const initializeBoard = () => {
+        for (let i = 0; i < ships.length; i++) {
+            let ship = ships[i];
+            // 1 is horizontal | 0 is vertical
+            let direction = Math.random() < 0.5 ? 1 : 0;
+            let x = Math.floor(Math.random() * 10);
+            let y = Math.floor(Math.random() * 10);
+            placeShip(ship, direction, x, y);
+        }
     };
 
     const receiveAttack = () => {
@@ -27,5 +92,5 @@ export const Gameboard = (n) => {
         return true;
     };
 
-    return { getBoard, reset, receiveAttack, allSunk }
+    return { getBoard, reset, initializeBoard, receiveAttack, allSunk };
 };
