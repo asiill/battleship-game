25ca21146e620dc33c94b5f924ecb4935524e657
asil <9745019+asiill@users.noreply.github.com>
2023-09-26 12:31:47 -0400

    Update player factory

--- src/player.js
+++ src/player.js
@@ -4,6 +4,7 @@ export const Player = (name, size) => {
     let n = size;
     let playerName = name;
     let gameboard = Gameboard(n);
+    let attacks = [];
 
     const getName = () => {
         return playerName;
@@ -17,5 +18,28 @@ export const Player = (name, size) => {
         gameboard.initializeBoard();
     };
 
-    return { getName, getGameboard, initializeGameboard };
+    const hasAttacked = (x, y) => {
+        for (let i = 0; i < attacks.length; i++) {
+            if (attacks[i][0] === x && attacks[i][1] === y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const attack = (x, y, board) => {
+        if (hasAttacked(x, y)) {
+            return;
+        }
+        attacks.push([x, y]);
+        board.receiveAttack(x, y);
+    }
+
+    const randomAttack = (board) => {
+        let x = Math.floor(Math.random() * 10);
+        let y = Math.floor(Math.random() * 10);
+        attack(x, y, board);
+    }
+
+    return { getName, getGameboard, initializeGameboard, attack, randomAttack};
 };
