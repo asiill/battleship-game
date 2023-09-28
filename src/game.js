382b5ce9f4c0c23b841efa0caa85e2c4a509d84f
asil <9745019+asiill@users.noreply.github.com>
2023-09-28 00:29:27 -0400

    Create game factory

--- src/game.js
+++ src/game.js
@@ -2,15 +2,15 @@ export const Game = (player, opponent) => {
     let winner;
 
     const isGameOver = () => {
-        if (player.getAttacks === 100 || opponent.getAttacks === 100) {
+        if (player.getAttacks().length === 100 || opponent.getAttacks().length === 100) {
             winner = "tie";
             return true;
         }
-        if (player.getGameboard.allSunk()) {
+        if (player.getGameboard().allSunk()) {
             winner = opponent.getName();
             return true;
         }
-        if (opponent.getGameboard.allSunk()) {
+        if (opponent.getGameboard().allSunk()) {
             winner = player.getName();
             return true;
         }
