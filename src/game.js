e04c495079f12410b979a2575dee82d4242f58fd
asil <9745019+asiill@users.noreply.github.com>
2023-09-28 23:44:00 -0400

    Update game.js

--- src/game.js
+++ src/game.js
@@ -2,10 +2,6 @@ export const Game = (player, opponent) => {
     let winner;
 
     const isGameOver = () => {
-        if (player.getAttacks().length === 100 || opponent.getAttacks().length === 100) {
-            winner = "tie";
-            return true;
-        }
         if (player.getGameboard().allSunk()) {
             winner = opponent.getName();
             return true;
